e8bb6b661104c10ad9b1bced64e561e6803f6bbf
Mohammad <mohammadezeldin55@gmail.com>
2019-04-25 17:11:06 +0200

    correct poblem at edit of medcial org

--- GP_EMR_Project/Controllers/Medical_OrganizationController.cs
+++ GP_EMR_Project/Controllers/Medical_OrganizationController.cs
@@ -104,12 +104,11 @@ namespace GP_EMR_Project.Controllers
                 medical_Organization.User.Phone = Request.Form["User.Phone"];
                 medical_Organization.User.City = Request.Form["User.City"];
                 medical_Organization.User.Address = Request.Form["User.Address"];
-                if (Request.Form["User.Password"] != medical_Organization.User.Password.Split(' ')[0])
+                if (Request.Form["User.Password"].Split(' ')[0] != medical_Organization.User.Password.Split(' ')[0])
                 {
-                    if (Request.Form["Confirm_Password"].Equals(Request.Form["User.Password"]))
+                    if (Request.Form["Confirm_Password"].Split(' ')[0] == Request.Form["User.Password"].Split(' ')[0])
                     {
-                        medical_Organization.User.Password = Request.Form["User.Password"];
-                        return View(medical_Organization);
+                        medical_Organization.User.Password = Request.Form["User.Password"]; 
                     }
                     else
                     {
